0a8eb365fd48b905038822b0a1b0f7547cc05ad3
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-07-08 01:40:33 +0530

    added customer profession list data

---
+++ src/data/profession.ts
@@ -0,0 +1,10 @@
+export const professionList = [
+    'school student',
+    'college student',
+    'teacher',
+    'software engineer',
+    'architect',
+    'social media manager',
+    'graphic designer',
+    'animator',
+];
