0aaa55a9e471a317a4f22c5587e4fd8afaac9994
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-07-09 02:55:17 +0530

    added content category data

---
+++ src/data/contentCategory.ts
@@ -0,0 +1,13 @@
+export const contentCategory = [
+    'email templates',
+    'ebooks',
+    'poster designs',
+    'canva template',
+    'business card designs',
+    'case studies',
+    'graphics',
+    'illustrations',
+    'vector art',
+    'codes',
+    'digital art',
+];
