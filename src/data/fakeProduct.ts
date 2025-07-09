f1dcee4767f7e03e9a0051dd95ce1c2cd31084b4
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-07-09 23:57:10 +0530

    added dummy product list data

---
+++ src/data/fakeProduct.ts
@@ -0,0 +1,99 @@
+export const productList = [
+    {
+        id: 1,
+        name: `Story of us`,
+        desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, tempore optio. Quam ut totam, doloribus sed officia recusandae commodi numquam aspernatur, atque quo repellendus modi impedit perspiciatis quidem veniam ducimus!
+          Soluta fuga facilis nisi enim eum, beatae libero ratione voluptatum doloremque! Ad omnis debitis cupiditate soluta sed esse voluptatem? Vero dolores modi delectus eos repellendus! Doloribus veritatis eligendi qui velit.
+          Quae, reiciendis atque. Deserunt veritatis labore doloribus sapiente nobis magnam sunt porro mollitia. Ex itaque dicta hic cum facilis praesentium asperiores vel vitae, consectetur, aut debitis dolores aliquam. Facere, amet.`,
+        img: `https://plus.unsplash.com/premium_photo-1722092223135-21fd4b5f9cc8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlnaXRhbCUyMHByb2R1Y3RzfGVufDB8fDB8fHww`,
+        rating: 3.3,
+        price: 3999,
+        category: `email templates`,
+        reviews: `13`
+    },
+    {
+        id: 2,
+        name: `Story of us`,
+        desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, tempore optio. Quam ut totam, doloribus sed officia recusandae commodi numquam aspernatur, atque quo repellendus modi impedit perspiciatis quidem veniam ducimus!
+          Soluta fuga facilis nisi enim eum, beatae libero ratione voluptatum doloremque! Ad omnis debitis cupiditate soluta sed esse voluptatem? Vero dolores modi delectus eos repellendus! Doloribus veritatis eligendi qui velit.
+          Quae, reiciendis atque. Deserunt veritatis labore doloribus sapiente nobis magnam sunt porro mollitia. Ex itaque dicta hic cum facilis praesentium asperiores vel vitae, consectetur, aut debitis dolores aliquam. Facere, amet.`,
+        img: `https://plus.unsplash.com/premium_photo-1722092223135-21fd4b5f9cc8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlnaXRhbCUyMHByb2R1Y3RzfGVufDB8fDB8fHww`,
+        rating: 4.0,
+        category: `ebooks`,
+        price: 450,
+        reviews: `13`
+    },
+    {
+        id: 3,
+        name: `Story of us`,
+        desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, tempore optio. Quam ut totam, doloribus sed officia recusandae commodi numquam aspernatur, atque quo repellendus modi impedit perspiciatis quidem veniam ducimus!
+          Soluta fuga facilis nisi enim eum, beatae libero ratione voluptatum doloremque! Ad omnis debitis cupiditate soluta sed esse voluptatem? Vero dolores modi delectus eos repellendus! Doloribus veritatis eligendi qui velit.
+          Quae, reiciendis atque. Deserunt veritatis labore doloribus sapiente nobis magnam sunt porro mollitia. Ex itaque dicta hic cum facilis praesentium asperiores vel vitae, consectetur, aut debitis dolores aliquam. Facere, amet.`,
+        img: `https://plus.unsplash.com/premium_photo-1722092223135-21fd4b5f9cc8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlnaXRhbCUyMHByb2R1Y3RzfGVufDB8fDB8fHww`,
+        rating: 4.9,
+        category: `case studies`,
+        price: 999,
+        reviews: `13`
+    },
+    {
+        id: 4,
+        name: `Story of us`,
+        desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, tempore optio. Quam ut totam, doloribus sed officia recusandae commodi numquam aspernatur, atque quo repellendus modi impedit perspiciatis quidem veniam ducimus!
+          Soluta fuga facilis nisi enim eum, beatae libero ratione voluptatum doloremque! Ad omnis debitis cupiditate soluta sed esse voluptatem? Vero dolores modi delectus eos repellendus! Doloribus veritatis eligendi qui velit.
+          Quae, reiciendis atque. Deserunt veritatis labore doloribus sapiente nobis magnam sunt porro mollitia. Ex itaque dicta hic cum facilis praesentium asperiores vel vitae, consectetur, aut debitis dolores aliquam. Facere, amet.`,
+        img: `https://plus.unsplash.com/premium_photo-1722092223135-21fd4b5f9cc8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlnaXRhbCUyMHByb2R1Y3RzfGVufDB8fDB8fHww`,
+        rating: 3.7,
+        category: `email templates`,
+        price: 12000,
+        reviews: `13`
+    },
+    {
+        id: 5,
+        name: `Story of us`,
+        desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, tempore optio. Quam ut totam, doloribus sed officia recusandae commodi numquam aspernatur, atque quo repellendus modi impedit perspiciatis quidem veniam ducimus!
+          Soluta fuga facilis nisi enim eum, beatae libero ratione voluptatum doloremque! Ad omnis debitis cupiditate soluta sed esse voluptatem? Vero dolores modi delectus eos repellendus! Doloribus veritatis eligendi qui velit.
+          Quae, reiciendis atque. Deserunt veritatis labore doloribus sapiente nobis magnam sunt porro mollitia. Ex itaque dicta hic cum facilis praesentium asperiores vel vitae, consectetur, aut debitis dolores aliquam. Facere, amet.`,
+        img: `https://plus.unsplash.com/premium_photo-1722092223135-21fd4b5f9cc8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlnaXRhbCUyMHByb2R1Y3RzfGVufDB8fDB8fHww`,
+        rating: 3.2,
+        category: `canva templates`,
+        price: 25,
+        reviews: `13`
+    },
+    {
+        id: 6,
+        name: `Story of us`,
+        desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, tempore optio. Quam ut totam, doloribus sed officia recusandae commodi numquam aspernatur, atque quo repellendus modi impedit perspiciatis quidem veniam ducimus!
+          Soluta fuga facilis nisi enim eum, beatae libero ratione voluptatum doloremque! Ad omnis debitis cupiditate soluta sed esse voluptatem? Vero dolores modi delectus eos repellendus! Doloribus veritatis eligendi qui velit.
+          Quae, reiciendis atque. Deserunt veritatis labore doloribus sapiente nobis magnam sunt porro mollitia. Ex itaque dicta hic cum facilis praesentium asperiores vel vitae, consectetur, aut debitis dolores aliquam. Facere, amet.`,
+        img: `https://plus.unsplash.com/premium_photo-1722092223135-21fd4b5f9cc8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlnaXRhbCUyMHByb2R1Y3RzfGVufDB8fDB8fHww`,
+        rating: 4,
+        category: `canva templates`,
+        price: 221,
+        reviews: `13`
+    },
+    {
+        id: 7,
+        name: `Story of us`,
+        desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, tempore optio. Quam ut totam, doloribus sed officia recusandae commodi numquam aspernatur, atque quo repellendus modi impedit perspiciatis quidem veniam ducimus!
+          Soluta fuga facilis nisi enim eum, beatae libero ratione voluptatum doloremque! Ad omnis debitis cupiditate soluta sed esse voluptatem? Vero dolores modi delectus eos repellendus! Doloribus veritatis eligendi qui velit.
+          Quae, reiciendis atque. Deserunt veritatis labore doloribus sapiente nobis magnam sunt porro mollitia. Ex itaque dicta hic cum facilis praesentium asperiores vel vitae, consectetur, aut debitis dolores aliquam. Facere, amet.`,
+        img: `https://plus.unsplash.com/premium_photo-1722092223135-21fd4b5f9cc8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlnaXRhbCUyMHByb2R1Y3RzfGVufDB8fDB8fHww`,
+        rating: 3.8,
+        category: `poster designs`,
+        price: 1939,
+        reviews: `13`
+    },
+    {
+        id: 8,
+        name: `Story of us`,
+        desc: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam, tempore optio. Quam ut totam, doloribus sed officia recusandae commodi numquam aspernatur, atque quo repellendus modi impedit perspiciatis quidem veniam ducimus!
+          Soluta fuga facilis nisi enim eum, beatae libero ratione voluptatum doloremque! Ad omnis debitis cupiditate soluta sed esse voluptatem? Vero dolores modi delectus eos repellendus! Doloribus veritatis eligendi qui velit.
+          Quae, reiciendis atque. Deserunt veritatis labore doloribus sapiente nobis magnam sunt porro mollitia. Ex itaque dicta hic cum facilis praesentium asperiores vel vitae, consectetur, aut debitis dolores aliquam. Facere, amet.`,
+        img: `https://plus.unsplash.com/premium_photo-1722092223135-21fd4b5f9cc8?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZGlnaXRhbCUyMHByb2R1Y3RzfGVufDB8fDB8fHww`,
+        rating: 4.2,
+        category: `vector art`,
+        price: 399,
+        reviews: `13`
+    },
+];
+
