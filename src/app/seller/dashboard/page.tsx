22e8968f43069c8ccd0606ae9ee88b9a3c38afbd
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-07-12 22:27:50 +0530

    added backend route protection

--- src/app/seller/dashboard/page.tsx
+++ src/app/seller/dashboard/page.tsx
@@ -1,16 +1,38 @@
 'use client'
 
 import SellerNavbar from "@/components/SellerNavbar"
-import { usePathname } from "next/navigation"
+import axios from "axios"
+import { usePathname, useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 function page() {
 
-    const pathname = usePathname();
+  const pathname = usePathname();
+  const router = useRouter();
+
+  useEffect(() => {
+    const verify = async () => {
+      try {
+        const res = await axios.get(`/api/seller/auth`, {
+          withCredentials: true
+        });
+
+        //console.log(res.data.data);
+        if (res.status !== 200) {
+          router.push('/denied');
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    verify();
+  }, []);
+  
   return (
     <>
       <div className={`w-full flex flex-col justify-start items-center relative overflow-hidden`}>
-            <SellerNavbar pathname={pathname}/>
+        <SellerNavbar pathname={pathname} />
       </div>
     </>
   )
