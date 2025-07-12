7a6cb59499ee1c934a702b62564dca0c154c4c6b
Sudipto Das <137734085+isudiptodas@users.noreply.github.com>
2025-07-12 22:23:07 +0530

    added backend verification

--- src/app/customer/auth/page.tsx
+++ src/app/customer/auth/page.tsx
@@ -7,6 +7,8 @@ import { FaRegEye } from "react-icons/fa";
 import { FaRegEyeSlash } from "react-icons/fa";
 import { FaCaretDown } from "react-icons/fa";
 import { professionList } from "@/data/profession";
+import { toast } from "sonner";
+import axios from 'axios';
 
 function page() {
 
@@ -14,15 +16,80 @@ function page() {
     const [visible, setVisible] = useState(false);
     const [profession, setProfession] = useState('choose');
     const [dropdownVisible, setDropdownVisible] = useState(false);
-    const[name, setName] = useState('');
-    const[email, setEmail] = useState('');
-    const[location, setLocation] = useState('');
-    const[password, setPassword] = useState('');
-    const[confirm, setConfirm] = useState('');
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [location, setLocation] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirm, setConfirm] = useState('');
     const router = useRouter();
+    const [loading, setLoading] = useState(false);
 
     const navigate = () => {
         router.push('/');
+    }
+
+    const register = async () => {
+        if (!email || !name || !profession || !password || !confirm || !location) {
+            toast.error("All fields are required");
+            return;
+        }
+
+        if (confirm !== password) {
+            toast.error("Passwords do not match");
+            return;
+        }
+
+        try {
+            setLoading(true);
+            const res = await axios.post(`/api/customer/auth`, {
+                name, email, password, profession, location, type: 'register'
+            });
+            if (res.status === 200) {
+                setLoading(false);
+            }
+        } catch (err: any) {
+            if (err?.response && err?.response?.data?.message) {
+                toast.error(err.response.data.message);
+            }
+            console.log(err);
+        }
+        finally {
+            setLoading(false);
+            setName('');
+            setEmail('');
+            setPassword('');
+            setLocation('');
+            setConfirm('');
+            setProfession('');
+        }
+    }
+
+    const login = async () => {
+        if (!email || !password) {
+            toast.error("Empty fields not allowed");
+            return;
+        }
+
+        try {
+            setLoading(true);
+            const res = await axios.post(`/api/customer/auth`, {
+                email, password, type: 'login'
+            });
+            if (res.status === 200) {
+                setLoading(false);
+                router.push('/customer/dashboard');
+            }
+        } catch (err: any) {
+            if (err?.response && err?.response?.data?.message) {
+                toast.error(err.response.data.message);
+            }
+            console.log(err);
+        }
+        finally {
+            setLoading(false);
+            setEmail('');
+            setPassword('');
+        }
     }
 
     return (
@@ -62,10 +129,10 @@ function page() {
                             <p onClick={() => setDropdownVisible(!dropdownVisible)} className={`w-full -mt-2 px-3 py-2 cursor-pointer rounded-lg bg-gray-200 text-start flex justify-between items-center`}>{profession} <FaCaretDown className={`${dropdownVisible ? "rotate-180" : "rotate-0"} duration-200 ease-in-out`} /></p>
                             <div className={`${dropdownVisible ? "block" : "hidden"} absolute top-5 overflow-y-auto w-full bg-gray-200 rounded-xl flex flex-col justify-start items-start px-1 py-1`}>
                                 {professionList.map((prof, index) => {
-                                    return <span key={index} className={`w-full text-start py-2 capitalize px-2 rounded-lg font-Kanit hover:bg-white duration-200 ease-in-out cursor-pointer`} onClick={() => {setProfession(prof); setDropdownVisible(false);}}>{prof}</span>
+                                    return <span key={index} className={`w-full text-start py-2 capitalize px-2 rounded-lg font-Kanit hover:bg-white duration-200 ease-in-out cursor-pointer`} onClick={() => { setProfession(prof); setDropdownVisible(false); }}>{prof}</span>
                                 })}
                             </div>
-                            <p className={`w-full rounded-full py-2 text-center bg-black text-white font-Kanit cursor-pointer`}>Submit</p>
+                            <p className={`w-full rounded-full py-2 text-center bg-black text-white font-Kanit cursor-pointer`} onClick={register}>{loading ? "Processing" : "Submit"}</p>
                         </div>
 
                         <div className={`w-full mt-10 ${option === 'login' ? "block" : "hidden"} h-full overflow-y-auto flex flex-col justify-start items-center gap-3`}>
@@ -74,7 +141,7 @@ function page() {
                                 <input value={password} onChange={(e) => setPassword(e.target.value)} type={visible ? "text" : "password"} placeholder="Enter password" className="w-full px-3 py-3 border-b-[1px] border-gray-500 outline-none font-Kanit" />
                                 <span onClick={() => setVisible(!visible)} className={`absolute top-1/2 right-5 -translate-y-1/2 opacity-40 cursor-pointer`}>{visible ? <FaRegEye /> : <FaRegEyeSlash />}</span>
                             </div>
-                            <p className={`w-full rounded-full py-2 text-center bg-black text-white font-Kanit cursor-pointer`}>Continue</p>
+                            <p className={`w-full rounded-full py-2 text-center bg-black text-white font-Kanit cursor-pointer`} onClick={login}>{loading ? "Processing" : "Continue"}</p>
                         </div>
                     </div>
                 </div>
